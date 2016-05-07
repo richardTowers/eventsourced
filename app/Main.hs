1cffa2cbd71c6ec8475df2af1aed91af07dd21cc
Richard Towers <richard@richard-towers.com>
2016-05-07 18:47:18 +0100

    Worked out about bytestrings and builders
    TODO: still waits for all of stdin before flushing the response :(. How
    to make it stream?

--- app/Main.hs
+++ app/Main.hs
@@ -2,8 +2,9 @@ module Main where
 
 import Network.Wai.Handler.Warp (run)
 import Lib (createWaiApp)
+import qualified Data.ByteString.Lazy as L
 
 main :: IO ()
 main = do
-    run 1337 $ createWaiApp getContents
+    run 1337 $ createWaiApp L.getContents
 
--- src/Lib.hs
+++ src/Lib.hs
@@ -2,9 +2,17 @@ module Lib ( createWaiApp) where
 
 import Network.Wai
 import Network.Wai.EventSource
+import qualified Data.ByteString.Lazy as L
+import qualified Data.ByteString.Lazy.Char8 as C
+import Blaze.ByteString.Builder.ByteString
 
-toEvent :: String -> ServerEvent
-toEvent s = CloseEvent
+toEvent :: [L.ByteString] -> ServerEvent
+toEvent s = ServerEvent {
+    eventName = Nothing,
+    eventId = Nothing,
+    eventData = map fromLazyByteString s
+}
 
-createWaiApp :: IO String -> Application
-createWaiApp input = eventSourceAppIO $ fmap toEvent input
+createWaiApp :: IO L.ByteString -> Application
+createWaiApp input = eventSourceAppIO $ fmap toEvent $ fmap C.lines input
+
