5bef621d0c07ddb398943a9989751c05091ecc84
Richard Towers <richard@richard-towers.com>
2016-05-11 23:59:48 +0100

    WIP: Removing IO from opt parsing
    Needs some refactoring - in particular the use of the T combinator
    should probably be removed (@benji6).

--- src/Args.hs
+++ src/Args.hs
@@ -1,37 +1,52 @@
-module Args (Options(..), getCommandLineOptions) where
+module Args (
+    Options(..),
+    getCommandLineOptions,
+    defaultOptions,
+    mergeOptions,
+    makePort,
+    makeAllowOrigin
+  ) where
 
-import Lib (application)
 import System.Environment (getArgs)
 import System.Exit (exitWith, ExitCode(..))
 import System.Console.GetOpt
 
+type OptionsTransformer = (Options -> Options)
+
 getCommandLineOptions :: IO Options
 getCommandLineOptions = do
     args <- getArgs
-    let (actions, _, _) = getOpt RequireOrder options args
-    foldl (>>=) defaultOptions actions
+    let (transformers, _, _) = getOpt RequireOrder options args
+    return $ mergeOptions transformers
 
-data Options   = Options { optPort :: Int, optAllowOrigin :: String }
-defaultOptions :: IO Options
-defaultOptions = return $ Options 1337 "null"
+mergeOptions :: [(OptionsTransformer)] -> Options
+mergeOptions = foldl t defaultOptions
 
-options :: [OptDescr (Options -> IO Options)]
+-- "T" ("Thrush") combinator from To Mock a Mockingbird
+t :: a -> (a -> b) -> b
+t x y = y x
+
+data Options = Options { optPort :: Int, optAllowOrigin :: String } deriving (Eq, Show)
+defaultOptions :: Options
+defaultOptions = Options 1337 "null"
+
+options :: [OptDescr (OptionsTransformer)]
 options = [
-    Option ['h'] ["help"] (NoArg showHelp) "show this help message",
+--    Option ['h'] ["help"] (NoArg showHelp) "show this help message",
     Option ['p'] ["port"] (ReqArg makePort "PORT") "send events to port (default 1337)",
     Option ['a'] ["allow-origin"] (ReqArg makeAllowOrigin "ORIGIN") "value for the Access-Control-Allow-Origin header (default null)"
   ]
 
-showHelp :: Options -> IO Options
-showHelp _ = do
-    putStrLn $ usageInfo header options
-    exitWith ExitSuccess
+-- showHelp :: Options -> IO Options
+-- showHelp _ = do
+--     putStrLn $ usageInfo header options
+--     exitWith ExitSuccess
 
-makePort :: String -> Options -> IO Options
-makePort s opt = return opt { optPort = (read s) }
+makePort :: String -> OptionsTransformer
+makePort s opt = opt { optPort = (read s) }
 
-makeAllowOrigin :: String -> Options -> IO Options
-makeAllowOrigin s opt = return opt { optAllowOrigin = s }
+makeAllowOrigin :: String -> OptionsTransformer
+makeAllowOrigin s opt = opt { optAllowOrigin = s }
 
 header :: String
 header = "Usage: eventsourced [OPTIONS...]"
--- test/Spec.hs
+++ test/Spec.hs
@@ -10,6 +10,7 @@ import Data.ByteString.Char8 (pack, unpack)
 import Data.ByteString.Builder (Builder)
 
 import Lib (serverEvent, createCorsHeaders)
+import Args (Options(..), makePort, defaultOptions)
 
 -- ServerEvent and Builder don't derive Eq or Show, which makes them hard to test.
 -- We can tell Haskell how to compare / show them by creating type instances:
@@ -32,11 +33,17 @@ testServerEventClosesWhenEndOfFileReached = do
 testCreateCorsHeaders = do
     TestCase $ assertEqual "for createCorsHeaders BANANA" (createCorsHeaders "BANANA") [("Access-Control-Allow-Origin", "BANANA")]
 
+testMakePort :: Test
+testMakePort = do
+    let port = makePort "0" defaultOptions
+    TestCase $ assertEqual "for makePort PORT 1337" port (defaultOptions { optPort = 0 })
+
 main :: IO ()
 main = do
     counts <- runTestTT $ test [
         testServerEvent,
         testServerEventClosesWhenEndOfFileReached,
-        testCreateCorsHeaders
+        testCreateCorsHeaders,
+        testMakePort
       ]
     putStrLn "Done"
