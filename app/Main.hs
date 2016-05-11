e47de9e66289a1dbc4ece3b8867bba544853c91b
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2016-05-11 17:52:45 +0100

    Move command line arg parsing into Args module
    ... instead of leaving it inline in Main.

--- app/Main.hs
+++ app/Main.hs
@@ -1,42 +1,12 @@
 module Main where
 
-import Network.Wai.Handler.Warp (run)
 import Lib (application)
-import System.Environment (getArgs)
-import System.Exit (exitWith, ExitCode(..))
-import System.Console.GetOpt
+import Args (Options(..), getCommandLineOptions)
+import Network.Wai.Handler.Warp (run)
 
 main :: IO ()
 main = do
-    args <- getArgs
-    let (actions, _, _) = getOpt RequireOrder options args
-    opts <- foldl (>>=) (return defaultOptions) actions
-    let Options { optPort = port, optAllowOrigin = allowOrigin } = opts
+    Options port allowOrigin <- getCommandLineOptions
     putStrLn $ "Streaming standard input to port " ++ (show port)
     putStrLn $ "Allowed orgins: " ++ allowOrigin
     run port $ application allowOrigin
-
-data Options   = Options { optPort :: Int, optAllowOrigin :: String }
-defaultOptions :: Options
-defaultOptions = Options 1337 "null"
-
-options :: [OptDescr (Options -> IO Options)]
-options = [
-    Option ['h'] ["help"] (NoArg showHelp) "show this help message",
-    Option ['p'] ["port"] (ReqArg makePort "PORT") "send events to port (default 1337)",
-    Option ['a'] ["allow-origin"] (ReqArg makeAllowOrigin "ORIGIN") "value for the Access-Control-Allow-Origin header (default null)"
-  ]
-
-showHelp :: Options -> IO Options
-showHelp _ = do
-    putStrLn $ usageInfo header options
-    exitWith ExitSuccess
-
-makePort :: String -> Options -> IO Options
-makePort s opt = return opt { optPort = (read s) }
-
-makeAllowOrigin :: String -> Options -> IO Options
-makeAllowOrigin s opt = return opt { optAllowOrigin = s }
-
-header :: String
-header = "Usage: eventsourced [OPTIONS...]"
---
+++ src/Args.hs
@@ -0,0 +1,37 @@
+module Args (Options(..), getCommandLineOptions) where
+
+import Lib (application)
+import System.Environment (getArgs)
+import System.Exit (exitWith, ExitCode(..))
+import System.Console.GetOpt
+
+getCommandLineOptions :: IO Options
+getCommandLineOptions = do
+    args <- getArgs
+    let (actions, _, _) = getOpt RequireOrder options args
+    foldl (>>=) (return defaultOptions) actions
+
+data Options   = Options { optPort :: Int, optAllowOrigin :: String }
+defaultOptions :: Options
+defaultOptions = Options 1337 "null"
+
+options :: [OptDescr (Options -> IO Options)]
+options = [
+    Option ['h'] ["help"] (NoArg showHelp) "show this help message",
+    Option ['p'] ["port"] (ReqArg makePort "PORT") "send events to port (default 1337)",
+    Option ['a'] ["allow-origin"] (ReqArg makeAllowOrigin "ORIGIN") "value for the Access-Control-Allow-Origin header (default null)"
+  ]
+
+showHelp :: Options -> IO Options
+showHelp _ = do
+    putStrLn $ usageInfo header options
+    exitWith ExitSuccess
+
+makePort :: String -> Options -> IO Options
+makePort s opt = return opt { optPort = (read s) }
+
+makeAllowOrigin :: String -> Options -> IO Options
+makeAllowOrigin s opt = return opt { optAllowOrigin = s }
+
+header :: String
+header = "Usage: eventsourced [OPTIONS...]"
