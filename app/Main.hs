a7800f3c58c6a612488848b63c93a83127889510
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2016-05-12 16:15:23 +0100

    Continue to purge IO from lib code
    Most of parsing command line args can be pure...

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,17 @@
 module Main where
 
 import Lib (application)
+import System.Environment (getArgs)
 import Args (Options(..), getCommandLineOptions)
 import Network.Wai.Handler.Warp (run)
 
 main :: IO ()
 main = do
-    Options port allowOrigin <- getCommandLineOptions
-    putStrLn $ "Streaming standard input to port " ++ (show port)
-    putStrLn $ "Allowed orgins: " ++ allowOrigin
-    run port $ application allowOrigin
+    args <- getArgs
+    let opts = getCommandLineOptions args
+    case opts of
+        Options _    _          (Just help)  -> putStrLn help
+        Options port allowOrigin Nothing     -> do
+            putStrLn $ "Streaming standard input to port " ++ (show port)
+            putStrLn $ "Allowed orgins: " ++ allowOrigin
+            run port $ application allowOrigin
--- src/Args.hs
+++ src/Args.hs
@@ -7,36 +7,35 @@ module Args (
     makeAllowOrigin
   ) where
 
-import System.Environment (getArgs)
-import System.Exit (exitWith, ExitCode(..))
 import System.Console.GetOpt
 
 type OptionsTransformer = (Options -> Options)
 
-getCommandLineOptions :: IO Options
-getCommandLineOptions = do
-    args <- getArgs
+getCommandLineOptions :: [String] -> Options
+getCommandLineOptions args = do
     let (transformers, _, _) = getOpt RequireOrder options args
-    return $ mergeOptions transformers
+    mergeOptions transformers
 
 mergeOptions :: [(OptionsTransformer)] -> Options
 mergeOptions = foldl (flip ($)) defaultOptions
 
-data Options = Options { optPort :: Int, optAllowOrigin :: String } deriving (Eq, Show)
+data Options = Options {
+    optPort :: Int,
+    optAllowOrigin :: String,
+    optHelp :: (Maybe String)
+} deriving (Eq, Show)
 defaultOptions :: Options
-defaultOptions = Options 1337 "null"
+defaultOptions = Options 1337 "null" Nothing
 
 options :: [OptDescr (OptionsTransformer)]
 options = [
---    Option ['h'] ["help"] (NoArg showHelp) "show this help message",
+    Option ['h'] ["help"] (NoArg showHelp) "show this help message",
     Option ['p'] ["port"] (ReqArg makePort "PORT") "send events to port (default 1337)",
     Option ['a'] ["allow-origin"] (ReqArg makeAllowOrigin "ORIGIN") "value for the Access-Control-Allow-Origin header (default null)"
   ]
 
--- showHelp :: Options -> IO Options
--- showHelp _ = do
---     putStrLn $ usageInfo header options
---     exitWith ExitSuccess
+showHelp :: Options -> Options
+showHelp opt = opt { optHelp = Just (usageInfo header options) }
 
 makePort :: String -> OptionsTransformer
 makePort s opt = opt { optPort = (read s) }
