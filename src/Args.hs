305ff8ab89ecc437af12f5e39f598128aa9bec03
Richard Towers <richard@richard-towers.com>
2016-05-12 22:59:32 +0100

    Further tidy command line arg parsing

--- src/Args.hs
+++ src/Args.hs
@@ -1,47 +1,35 @@
-module Args (
-    Options(..),
-    getCommandLineOptions,
-    defaultOptions,
-    mergeOptions,
-    makePort,
-    makeAllowOrigin
-  ) where
+module Args (Options(..), getCommandLineOptions) where
 
 import System.Console.GetOpt
 
 type OptionsTransformer = (Options -> Options)
-
-getCommandLineOptions :: [String] -> Options
-getCommandLineOptions args = do
-    let (transformers, _, _) = getOpt RequireOrder options args
-    mergeOptions transformers
-
-mergeOptions :: [(OptionsTransformer)] -> Options
-mergeOptions = foldl (flip ($)) defaultOptions
 
 data Options = Options {
     optPort :: Int,
     optAllowOrigin :: String,
     optHelp :: (Maybe String)
 } deriving (Eq, Show)
+
 defaultOptions :: Options
 defaultOptions = Options 1337 "null" Nothing
 
+getCommandLineOptions :: [String] -> Options
+getCommandLineOptions args = do
+    let (transformers, _, _) = getOpt RequireOrder options args
+    foldl (flip ($)) defaultOptions transformers
+
 options :: [OptDescr (OptionsTransformer)]
 options = [
-    Option ['h'] ["help"] (NoArg showHelp) "show this help message",
+    Option ['h'] ["help"] (NoArg makeHelp) "show this help message",
     Option ['p'] ["port"] (ReqArg makePort "PORT") "send events to port (default 1337)",
     Option ['a'] ["allow-origin"] (ReqArg makeAllowOrigin "ORIGIN") "value for the Access-Control-Allow-Origin header (default null)"
   ]
 
-showHelp :: Options -> Options
-showHelp opt = opt { optHelp = Just (usageInfo header options) }
+makeHelp :: OptionsTransformer
+makeHelp opt = opt { optHelp = Just (usageInfo  "Usage: eventsourced [OPTIONS...]" options) }
 
 makePort :: String -> OptionsTransformer
 makePort s opt = opt { optPort = (read s) }
 
 makeAllowOrigin :: String -> OptionsTransformer
 makeAllowOrigin s opt = opt { optAllowOrigin = s }
-
-header :: String
-header = "Usage: eventsourced [OPTIONS...]"
