ee0c1161b91c90ede024b8906ba1cc9e551a57ad
Richard Towers <richard@richard-towers.com>
2016-05-15 11:43:29 +0100

    Shutdown the server once EOF is reached
    Bump version to 1.1

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import Control.Concurrent (forkIO, newEmptyMVar, takeMVar)
 import Network.Eventsourced.Lib (application)
 import Network.Eventsourced.Args (Options(..), getCommandLineOptions)
 
@@ -9,10 +10,13 @@ import Network.Wai.Handler.Warp (run)
 main :: IO ()
 main = do
     args <- getArgs
+    shutdownMVar <- newEmptyMVar
     let opts = getCommandLineOptions args
     case opts of
         Options _    _          (Just help)  -> putStrLn help
         Options port allowOrigin Nothing     -> do
             putStrLn $ "Streaming standard input to port " ++ (show port)
             putStrLn $ "Allowed orgins: " ++ allowOrigin
-            run port $ application allowOrigin
+            let app = application shutdownMVar allowOrigin
+            _ <- forkIO $ run port app
+            takeMVar shutdownMVar
--- src/Network/Eventsourced/Lib.hs
+++ src/Network/Eventsourced/Lib.hs
@@ -1,5 +1,6 @@
 module Network.Eventsourced.Lib (application, serverEvent, createCorsHeaders) where
 
+import Control.Concurrent (MVar, putMVar)
 import System.IO.Error (tryIOError)
 import Network.Wai (Middleware, Application)
 import Network.Wai.EventSource (ServerEvent(..), eventSourceAppIO)
@@ -12,10 +13,15 @@ serverEvent :: Either IOError String -> ServerEvent
 serverEvent (Left _) = CloseEvent
 serverEvent (Right s) = ServerEvent Nothing Nothing [ fromString s ]
 
-eventFromLine :: IO ServerEvent
-eventFromLine = do
+eventFromLine :: MVar () -> IO ServerEvent
+eventFromLine shutdownMVar = do
     input <- tryIOError getLine
-    return $ serverEvent input
+    let event = serverEvent input
+    case event of
+        CloseEvent -> do
+            putMVar shutdownMVar ()
+            return event
+        _ -> return event
 
 createCorsHeaders :: String -> [(ByteString, ByteString)]
 createCorsHeaders s = [(pack "Access-Control-Allow-Origin", pack s)]
@@ -23,5 +29,7 @@ createCorsHeaders s = [(pack "Access-Control-Allow-Origin", pack s)]
 addCorsHeaders :: String -> Middleware
 addCorsHeaders s = addHeaders $ createCorsHeaders s
 
-application :: String -> Application
-application allowOrigin = addCorsHeaders allowOrigin $ eventSourceAppIO eventFromLine
+application :: MVar () -> String -> Application
+application shutdown allowOrigin = do
+    let app = eventSourceAppIO $ eventFromLine shutdown
+    addCorsHeaders allowOrigin app
