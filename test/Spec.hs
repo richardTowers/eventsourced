5795e5083c0d3273e36f293c4597f99bf2ee5bd5
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2016-05-19 13:06:26 +0100

    Fix hlint warnings in tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -17,11 +17,11 @@ import Network.Eventsourced.Args (Options(..), getCommandLineOptions, defaultOpt
 -- ServerEvent and Builder don't derive Eq or Show, which makes them hard to test.
 -- We can tell Haskell how to compare / show them by creating type instances:
 deriving instance Show ServerEvent
-instance Show (Builder) where show = unpack . toByteString
+instance Show Builder where show = unpack . toByteString
 deriving instance Eq ServerEvent
 instance Eq Builder where
-    (==) x y = (show x) == (show y)
-    (/=) x y = (show x) /= (show y)
+    (==) x y = show x == show y
+    (/=) x y = show x /= show y
 
 testServerEvent :: Test
 testServerEvent = do
@@ -35,11 +35,11 @@ testServerEventClosesWhenEndOfFileReached = do
     TestCase $ assertEqual "for serverEvent (Left IOError)" (serverEvent endOfFileError) CloseEvent
 
 testCreateCorsHeaders :: Test
-testCreateCorsHeaders = do
+testCreateCorsHeaders =
     TestCase $ assertEqual "for createCorsHeaders BANANA" (createCorsHeaders "BANANA") [("Access-Control-Allow-Origin", "BANANA")]
 
 testGetCommandLineOptions :: Test
-testGetCommandLineOptions = do
+testGetCommandLineOptions =
     TestCase $ do
         assertEqual "getCommandLineOptions []" (getCommandLineOptions []) defaultOptions
         assertEqual "getCommandLineOptions [--port=12345]" (getCommandLineOptions ["--port=12345"]) (defaultOptions { optPort = 12345 })
@@ -47,9 +47,9 @@ testGetCommandLineOptions = do
         assertEqual "getCommandLineOptions [--port=12345 --allow-origin=*]" (getCommandLineOptions ["--port=12345", "--allow-origin=*"]) (defaultOptions {  optPort = 12345, optAllowOrigin = "*" })
 
 main :: IO ()
-main = void $ runTestTT $ test [
-    testServerEvent,
-    testServerEventClosesWhenEndOfFileReached,
-    testCreateCorsHeaders,
-    testGetCommandLineOptions
+main = void $ runTestTT $ test
+  [ testServerEvent
+  , testServerEventClosesWhenEndOfFileReached
+  , testCreateCorsHeaders
+  , testGetCommandLineOptions
   ]
